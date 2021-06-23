94b2a8b880064001d59c7bd8cd8f94bd707a6bab
Matheus França <mateusanches2@gmail.com>
2021-06-23 12:47:15 -0300

    Adding Express / EJS

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const routes = require('./routes')
+const path = require('path')
+const server  = express()
+
+server.set('view engine', 'ejs')
+
+server.use(express.static('public'))
+
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(routes)
+
+server.listen(4000, () => {
+  console.log('listening on port 4000')
+  }
+)
