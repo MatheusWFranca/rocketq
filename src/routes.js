2cbb7de99b360e64ccff54512a8c981436573c8d
Matheus França <mateusanches2@gmail.com>
2021-06-24 19:13:15 -0300

    SQLITE

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const QuestionController = require('./controllers/QuestionController');
+const RoomController = require('./controllers/RoomController');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {page: 'enter-room'})
 })
+routes.get('/room/:room', (req, res) => { res
+  res.render('room');
+})
+routes.get('/create-pass', (req, res) => {
+  res.render('index', {page: 'create-pass'});
+})
+
+routes.post('/question/:room/:question/:action', QuestionController.index)
+routes.post('/create-room', RoomController.create)
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const routes = require('./routes')
-const path = require('path')
-const server  = express()
+const express = require('express');
+const routes = require('./routes');
+const path = require('path');
+const server  = express();
 
-server.set('view engine', 'ejs')
+server.set('view engine', 'ejs');
 
-server.use(express.static('public'))
+server.use(express.static('public'));
 
-server.set('views', path.join(__dirname, 'views'))
+server.set('views', path.join(__dirname, 'views'));
 
-server.use(routes)
+server.use(express.urlencoded({extended: true}));
+
+server.use(routes);
 
 server.listen(4000, () => {
   console.log('listening on port 4000')
   }
-)
+);
